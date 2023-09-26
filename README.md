5bb5cb7b7458ee454cf4bef4f0ea12e547bc6063
yanglee2421 <yanglee2421@gmail.com>
2023-09-26 23:52:30 +0800

    flatlist

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Native
+
+## 平台差异
+
+1. Text 组件在 IOS 上不支持 borderRadius（包裹一层 View）
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,19 +1,53 @@
 // RN Imports
-import { Button, StyleSheet, TextInput, View, Text } from "react-native";
+import { useMemo, useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  StyleProp,
+  ViewStyle,
+  ScrollView,
+} from "react-native";
 
 export function Home() {
+  const [text, setText] = useState("");
+  const [goals, setGoals] = useState<string[]>([]);
+
+  const handleAddGoals = () => {
+    setGoals((prev) => Array.from(new Set<string>([text, ...prev])));
+  };
+
+  const goalsEl = useMemo(() => {
+    return Array.from(new Set(goals)).map((item, idx, list) => {
+      const itemStyle: StyleProp<ViewStyle> = {
+        ...styles.goalsItem,
+        marginBottom: idx === list.length - 1 ? 0 : 8,
+      };
+
+      return (
+        <View key={item} style={itemStyle}>
+          <Text style={styles.goalsText}>{item}</Text>
+        </View>
+      );
+    });
+  }, [goals]);
+
   return (
     <View style={styles.page}>
       <View style={styles.inputBox}>
         <TextInput
+          value={text}
+          onChangeText={setText}
           keyboardType="default"
           placeholder="Your course goal!"
           style={styles.textInput}
         />
-        <Button title="Add Goal" />
+        <Button onPress={handleAddGoals} title="Add Goal" />
       </View>
       <View style={styles.goalsBox}>
-        <Text>List of goals...</Text>
+        <ScrollView alwaysBounceVertical={true}>{goalsEl}</ScrollView>
       </View>
     </View>
   );
@@ -31,6 +65,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
     paddingBottom: 24,
+    marginBottom: 24,
     borderBottomColor: "#ccc",
     borderBottomWidth: 1,
   },
@@ -42,5 +77,17 @@ const styles = StyleSheet.create({
   },
   goalsBox: {
     flex: 1,
+    // borderWidth: 1,
+    // borderColor: "red",
+  },
+  goalsItem: {
+    padding: 8,
+    // margin: 8,
+    marginBottom: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  goalsText: {
+    color: "#fff",
   },
 });
