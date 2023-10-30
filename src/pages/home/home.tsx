3690c98bb91d24f5b5ebf43d2e822a5adc170f5b
ZhangSheng <yanglee2421@gmail.com>
2023-10-30 17:47:24 +0800

    Add eslint

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -15,26 +15,13 @@ import {
 export function Home() {
   const [text, setText] = useState("");
 
-  const [goals, setGoals] = useState<string[]>([
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-  ]);
+  const [goals, setGoals] = useState<string[]>(() => {
+    const list: string[] = [];
+    for (let i = 0; i < 20; i++) {
+      list.push(i.toString());
+    }
+    return list;
+  });
 
   const goalsEl = useMemo(() => {
     return goals.map((item, index) => {
