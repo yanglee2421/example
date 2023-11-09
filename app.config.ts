fd44f2b945e6698e430f736a3a25762b171a0a78
ZhangSheng <yanglee2421@gmail.com>
2023-11-09 09:23:06 +0800

    Add app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,12 @@
+import { ExpoConfig, ConfigContext } from "expo/config";
+
+export default (configCtx: ConfigContext): ExpoConfig => {
+  // ** ConfigContext
+  const { config } = configCtx;
+
+  return {
+    ...config,
+    slug: "my-app",
+    name: "My App",
+  };
+};
