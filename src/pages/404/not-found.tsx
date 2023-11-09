cf60797b15b1f6ceb9c03a7375d3a06f7e2732de
yanglee2421 <yanglee2421@gmail.com>
2023-11-10 01:33:29 +0800

    abortcontroller

--- src/pages/404/not-found.tsx
+++ src/pages/404/not-found.tsx
@@ -1,6 +1,37 @@
 // Expo Imports
-import { useFonts } from "expo-font";
+// import { useFonts } from "expo-font";
+
+// React Imports
+import React from "react";
+
+// RN Imports
+import {
+  AccessibilityInfo,
+  RefreshControl,
+  ScrollView,
+  Keyboard,
+} from "react-native";
 
 export function NotFound() {
-  return <></>;
+  React.useEffect(() => {
+    const subscription = AccessibilityInfo.addEventListener(
+      "reduceMotionChanged",
+      (evt) => {}
+    );
+
+    const sub = Keyboard.addListener("keyboardDidShow", (evt) => {});
+
+    fetch("", { signal: new AbortController().signal });
+
+    return () => {
+      subscription.remove();
+      sub.remove();
+    };
+  }, []);
+
+  return (
+    <ScrollView
+      refreshControl={<RefreshControl refreshing onRefresh={() => {}} />}
+    ></ScrollView>
+  );
 }
