c46d348a0574dd8c03b2917d44e849d3744b910c
ZhangSheng <yanglee2421@gmail.com>
2023-11-27 18:05:08 +0800

    Config RN Query

--- src/api/provider/query-client.ts
+++ src/api/provider/query-client.ts
@@ -1,9 +1,16 @@
 // Query Imports
-import { QueryClient, DefaultOptions } from "@tanstack/react-query";
+import {
+  QueryClient,
+  DefaultOptions,
+  onlineManager,
+  focusManager,
+} from "@tanstack/react-query";
 import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
 
 // RN Storage Imports
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import NetInfo from "@react-native-community/netinfo";
 
 // ** QueryClient
 export const queryClient = new QueryClient({
@@ -17,6 +24,15 @@ export const persister = createAsyncStoragePersister({
   storage: AsyncStorage,
   key: "rn-query-persister",
 });
+
+// Refetch on reconnect
+onlineManager.setEventListener((setOnline) => {
+  return NetInfo.addEventListener((state) => {
+    setOnline(Boolean(state.isConnected));
+  });
+});
+
+// Refetch on focus
 
 // ** Defaults
 queryClient.setQueryDefaults(["unique"], {
@@ -36,8 +52,8 @@ function queries(): DefaultOptions["queries"] {
     staleTime: 1000 * 60,
     gcTime: 1000 * 60 * 2,
     refetchOnMount: true,
-    refetchOnWindowFocus: true,
     refetchOnReconnect: true,
+    refetchOnWindowFocus: true,
     retryDelay(attemptIndex) {
       return Math.min(1000 * 2 ** attemptIndex, 30000);
     },
