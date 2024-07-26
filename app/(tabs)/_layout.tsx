746551b65102505993d1c0075b94dc9e21e1731c
yanglee2421 <yanglee2421@gmail.com>
2024-07-27 01:54:52 +0800

    feat: add rneui

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,28 @@
+import { FontAwesome } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+
+export default function TabsLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="cog" color={color} />
+          )
+        }}
+      />
+    </Tabs>
+  );
+}
