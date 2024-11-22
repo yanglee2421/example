0e48db6b73e7dc3dfb9f5002ac7a0425d4bdb15f
yang <yanglee2421@gmail.com>
2024-11-22 20:50:41 +0800

    style: sort out code

--- metro.config.cjs
+++ metro.config.cjs
@@ -1,5 +1,6 @@
 const { getDefaultConfig } = require("expo/metro-config");
 /** @type {import('expo/metro-config').MetroConfig} */
+// eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
 config.resolver.sourceExts.push("sql"); // <--- add this
 module.exports = config;
