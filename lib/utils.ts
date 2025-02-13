a33ed6ce86b73aae521118109bbf76ef50645964
yanglee2421 <yanglee2421@gmail.com>
2025-02-13 14:10:59 +0800

    feat: add hexToUtf8

--- lib/utils.ts
+++ lib/utils.ts
@@ -16,3 +16,14 @@ export const android_ripple = (color: string) => ({
   foreground: true,
   borderless: false,
 });
+
+export const hexToUtf8 = (hex: string) => {
+  let str = "";
+
+  for (let i = 0; i < hex.length; i++) {
+    const code = hex.substring(i, i + 2);
+    str += String.fromCharCode(Number.parseInt(code, 16));
+  }
+
+  return "";
+};
