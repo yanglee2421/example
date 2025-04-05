24230a0cf88a7efbd4c8baf01e7985c6c12aa061
yanglee2421 <yanglee2421@gmail.com>
2025-04-05 17:28:04 +0800

    style: sort out code

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,8 @@
-import type { Config } from "drizzle-kit";
-export default {
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
   schema: "./db/schema.ts",
   out: "./drizzle",
   dialect: "sqlite",
   driver: "expo",
-} satisfies Config;
+});
