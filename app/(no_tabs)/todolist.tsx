0ff00a8fa3f5cf7762a4146d45ce4388947de600
yang <yanglee2421@gmail.com>
2025-10-21 22:48:10 +0800

    wip[todolist]: Add animation to skia

--- app/(no_tabs)/todolist.tsx
+++ app/(no_tabs)/todolist.tsx
@@ -1,29 +1,88 @@
 import React from "react";
-import { Canvas, Circle, Group } from "@shopify/react-native-skia";
-import {
-  useDerivedValue,
-  useSharedValue,
-  withRepeat,
-  withTiming,
-} from "react-native-reanimated";
+import { Canvas, Path, usePathValue } from "@shopify/react-native-skia";
+import { useSharedValue, withSpring } from "react-native-reanimated";
+import { Button, View } from "react-native";
+import { GestureDetector, Gesture } from "react-native-gesture-handler";
+
+// path.close();
+
+type Point = {
+  x: number;
+  y: number;
+};
 
 const HelloWorld = () => {
-  const size = 256;
-  const r = useSharedValue(0);
-  const c = useDerivedValue(() => size - r.value);
+  const points = useSharedValue<Point[]>([]);
+  const cursorX = useSharedValue(0);
 
-  React.useEffect(() => {
-    r.value = withRepeat(withTiming(size * 0.33, { duration: 1000 }), -1);
-  }, [r, size]);
+  const path = usePathValue((path) => {
+    "worklet";
+    path.moveTo(0, 300);
+    points.get().forEach((point) => {
+      path.lineTo(point.x, point.y);
+    });
+  });
+
+  const cursorPath = usePathValue((path) => {
+    "worklet";
+    const x = cursorX.get();
+    path.moveTo(x, 0);
+    path.lineTo(x, 300);
+  });
+
+  const panGestureHandler = Gesture.Pan()
+    .onBegin((e) => {
+      cursorX.set(e.x);
+    })
+    .onStart((e) => {
+      cursorX.set(e.x);
+    })
+    .onUpdate((e) => {
+      cursorX.set(e.x);
+    })
+    .onEnd((e) => {
+      cursorX.set(e.x);
+    })
+    .onFinalize((e) => {
+      cursorX.set(withSpring(0));
+    });
 
   return (
-    <Canvas style={{ flex: 1 }}>
-      <Group blendMode="multiply">
-        <Circle cx={r} cy={r} r={r} color="cyan" />
-        <Circle cx={c} cy={r} r={r} color="magenta" />
-        <Circle cx={size / 2} cy={c} r={r} color="yellow" />
-      </Group>
-    </Canvas>
+    <>
+      <View style={{ height: 300, borderWidth: 1 }}>
+        <GestureDetector gesture={panGestureHandler}>
+          <Canvas style={{ flex: 1 }}>
+            <Path
+              path={path}
+              color="lightblue"
+              style={"stroke"}
+              strokeWidth={2}
+            />
+            <Path
+              path={cursorPath}
+              color="red"
+              style={"stroke"}
+              strokeWidth={2}
+            />
+          </Canvas>
+        </GestureDetector>
+      </View>
+
+      <Button
+        title="click me"
+        onPress={() => {
+          points.set((prev) => {
+            return [
+              ...prev,
+              {
+                x: prev.length * 10,
+                y: Math.floor(Math.random() * 300),
+              },
+            ];
+          });
+        }}
+      />
+    </>
   );
 };
 
