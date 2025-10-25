2804adbdb0f0841902e814b673ff4d280e75991d
yang <yanglee2421@gmail.com>
2025-10-25 15:06:34 +0800

    feat[settings]: Add Clipboard support

--- app/(no_tabs)/settings.tsx
+++ app/(no_tabs)/settings.tsx
@@ -1,12 +1,20 @@
-import { Button, View } from "react-native";
+import { Button, View, Pressable, Text, ToastAndroid } from "react-native";
 import * as fs from "expo-file-system";
+import * as clipboard from "expo-clipboard";
 import * as consts from "@/lib/constants";
+import { useTheme } from "@/hooks/useTheme";
+import { android_ripple } from "@/lib/utils";
 
 const getSQLiteFile = () => {
   return new fs.File(fs.Paths.document, `SQLite/${consts.databaseName}`);
 };
 
+const getURL = () =>
+  "https://y7k0k.no-mad-world.club/link/DgBCnmSmioWwDika?clash=3&extend=1";
+
 export default function Page() {
+  const theme = useTheme();
+
   return (
     <View>
       <Button
@@ -37,6 +45,34 @@ export default function Page() {
           sqliteFile.exists && sqliteFile.delete();
         }}
       />
+      <Pressable
+        onPress={async () => {
+          await clipboard.setStringAsync(getURL());
+          ToastAndroid.show("Copied", 1000 * 2);
+        }}
+        style={[
+          {
+            borderWidth: 1,
+            borderColor: "transparent",
+            borderBlockEndColor: theme.palette.divider,
+
+            paddingInline: theme.spacing(6),
+            paddingBlock: theme.spacing(3),
+          },
+        ]}
+        android_ripple={android_ripple(theme.palette.action.focus)}
+      >
+        <Text
+          style={[
+            theme.typography.body1,
+            {
+              color: theme.palette.text.primary,
+            },
+          ]}
+        >
+          {getURL()}
+        </Text>
+      </Pressable>
     </View>
   );
 }
