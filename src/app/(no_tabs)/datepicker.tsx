9155a3a86a4284609c7370365373de7fb8c87511
yang <yanglee2421@gmail.com>
2026-06-02 15:53:01 +0800

    feat: Date picker finished

--- src/app/(no_tabs)/datepicker.tsx
+++ src/app/(no_tabs)/datepicker.tsx
@@ -1,109 +1,126 @@
 import { AppHeader } from "@/components/app-header";
 import { Column, Host, Icon, Text } from "@expo/ui";
 import {
+  Box,
+  Card,
   DatePickerDialog,
-  DateTimePicker,
   IconButton,
   OutlinedTextField,
   Surface,
   useNativeState,
 } from "@expo/ui/jetpack-compose";
-import { fillMaxWidth } from "@expo/ui/jetpack-compose/modifiers";
+import { fillMaxWidth, padding } from "@expo/ui/jetpack-compose/modifiers";
+import dayjs from "dayjs";
 import React from "react";
-import { runOnUISync } from "react-native-worklets";
+import { scheduleOnUI } from "react-native-worklets";
 
-export default function DatePickerPage() {
-  const [selectedDate, setSelectedDate] = React.useState(() => new Date());
+interface DateFieldProps {
+  value: Date;
+  onChange: (date: Date) => void;
+  label: string;
+  helperText: string;
+}
+
+const DateField = (props: DateFieldProps) => {
+  const { value, onChange, label, helperText } = props;
 
   const [show, setShow] = React.useState(false);
-  const nativeText = useNativeState(selectedDate.toLocaleDateString());
+
+  const nativeText = useNativeState(value.toLocaleDateString());
+
   const handleChange = React.useCallback(
-    (v: string) => {
+    (text: string) => {
       "worklet";
-      nativeText.value = v;
+      nativeText.value = text;
     },
     [nativeText],
   );
 
   return (
+    <>
+      <OutlinedTextField
+        value={nativeText}
+        onValueChange={handleChange}
+        readOnly
+        singleLine
+        modifiers={[fillMaxWidth()]}
+      >
+        <OutlinedTextField.Label>
+          <Text>{label}</Text>
+        </OutlinedTextField.Label>
+        <OutlinedTextField.Placeholder>
+          <Text>Placeholder</Text>
+        </OutlinedTextField.Placeholder>
+        <OutlinedTextField.SupportingText>
+          <Text>{helperText}</Text>
+        </OutlinedTextField.SupportingText>
+        <OutlinedTextField.TrailingIcon>
+          <IconButton onClick={() => setShow(true)}>
+            <Icon
+              name={Icon.select({
+                ios: "0.circle",
+                android: import("@expo/material-symbols/event.xml"),
+              })}
+            />
+          </IconButton>
+        </OutlinedTextField.TrailingIcon>
+      </OutlinedTextField>
+      {show && (
+        <DatePickerDialog
+          onDismissRequest={() => {
+            setShow(false);
+          }}
+          initialDate={value.toISOString()}
+          onDateSelected={(date) => {
+            onChange(date);
+            setShow(false);
+            scheduleOnUI(handleChange, date.toLocaleDateString());
+          }}
+        />
+      )}
+    </>
+  );
+};
+
+export default function DatePickerPage() {
+  const [begin, setBegin] = React.useState(() => new Date());
+  const [end, setEnd] = React.useState(() => new Date());
+
+  const calcDayCount = (begin: Date, end: Date) => {
+    const count = dayjs(end).diff(dayjs(begin), "day") + 1;
+
+    return count;
+  };
+
+  return (
     <Host style={{ flex: 1 }}>
       <Surface>
-        <Column>
+        <Column spacing={12}>
           <AppHeader pageName="Date Picker" />
-          <OutlinedTextField
-            value={nativeText}
-            onValueChange={handleChange}
-            readOnly
-            singleLine
-            modifiers={[fillMaxWidth()]}
-          >
-            <OutlinedTextField.Label>
-              <Text>label</Text>
-            </OutlinedTextField.Label>
-            <OutlinedTextField.Placeholder>
-              <Text>Placeholder</Text>
-            </OutlinedTextField.Placeholder>
-            <OutlinedTextField.SupportingText>
-              <Text>SupportingText</Text>
-            </OutlinedTextField.SupportingText>
-            <OutlinedTextField.LeadingIcon>
-              <IconButton>
-                <Icon
-                  name={Icon.select({
-                    ios: "0.circle",
-                    android: import("@expo/material-symbols/account_tree.xml"),
-                  })}
-                />
-              </IconButton>
-            </OutlinedTextField.LeadingIcon>
-            <OutlinedTextField.TrailingIcon>
-              <IconButton onClick={() => setShow(true)}>
-                <Icon
-                  name={Icon.select({
-                    ios: "0.circle",
-                    android:
-                      import("@expo/material-symbols/calendar_month.xml"),
-                  })}
-                />
-              </IconButton>
-            </OutlinedTextField.TrailingIcon>
-            <OutlinedTextField.Prefix>
-              <Text>$</Text>
-            </OutlinedTextField.Prefix>
-            <OutlinedTextField.Suffix>
-              <Text>USD</Text>
-            </OutlinedTextField.Suffix>
-          </OutlinedTextField>
-        </Column>
-        {show && (
-          <DatePickerDialog
-            onDismissRequest={() => {
-              setShow(false);
-            }}
-            initialDate={selectedDate.toISOString()}
-            onDateSelected={(date) => {
-              setSelectedDate(date);
-              setShow(false);
-              const dateString = date.toLocaleDateString();
-
-              runOnUISync(() => {
-                handleChange(dateString);
-              });
-            }}
-          />
-        )}
-        <>
-          {false && (
-            <DateTimePicker
-              initialDate={selectedDate.toISOString()}
-              onDateSelected={(date) => {
-                setSelectedDate(date);
-              }}
-              displayedComponents="date"
-              variant="picker"
+          <Card modifiers={[padding(12, 0, 12, 0), fillMaxWidth()]}>
+            <Column style={{ padding: 12 }}>
+              <Text textStyle={{ fontSize: 20 }}>
+                {`共${calcDayCount(begin, end)}天`}
+              </Text>
+            </Column>
+          </Card>
+          <Box modifiers={[padding(12, 0, 12, 0)]}>
+            <DateField
+              value={begin}
+              onChange={setBegin}
+              label="开始日期"
+              helperText="请选择开始日期"
             />
-          )}
-        </>
+          </Box>
+          <Box modifiers={[padding(12, 0, 12, 0)]}>
+            <DateField
+              value={end}
+              onChange={setEnd}
+              label="结束日期"
+              helperText="请选择结束日期"
+            />
+          </Box>
+        </Column>
       </Surface>
     </Host>
   );
